cf43184d58ccf5624f120e0d5d9806f41b352ab1
Yuriy Puris <ypuris@itera-research.com>
2020-08-18 18:31:08 +0300

    profile

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { useProfilePictureStyles } from "../../styles";
+import { Person } from '@material-ui/icons';
 
-function ProfilePicture() {
-  useProfilePictureStyles();
+const ProfilePicture = ({ 
+  isOwner,
+  size,
+  image = "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF",
 
-  return <div>ProfilePicture</div>;
+}) => {
+  const classes = useProfilePictureStyles({ size, isOwner });
+
+  return (
+    <section className={classes.section}>
+      {
+        image ? (
+          <div className={classes.wrapper}>
+            <img src={image} alt="user profile" className={classes.image} />
+          </div>
+        ) : (
+          <div className={classes.wrapper}>
+            <Person className={classes.person} />
+          </div>
+        )
+      }
+    </section>
+  )
 }
 
 export default ProfilePicture;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,69 @@
 import React from "react";
 import { useProfilePageStyles } from "../styles";
 import Layout from '../components/shared/Layout';
+import { defaultCurrentUser } from '../data';
+import { Hidden, Card, CardContent } from '@material-ui/core';
+import ProfilePicture from '../components/shared/ProfilePicture';
 
-function ProfilePage() {
-  useProfilePageStyles();
+const ProfilePage = () => {
+  const classes = useProfilePageStyles();
+  const isOwner = true;
+  const [showOptionsMenu, setOptionsMenu] = React.useState(false);
 
-  return<Layout>Profile page</Layout>
-}
+  const handleOptionsMenuClick = () => {
+    setOptionsMenu(true);
+  };
+
+  return (
+    <Layout title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})`}>
+      <div className={classes.container}>
+        <Hidden xsDown>
+          <Card className={classes.cardLarge}>
+            <ProfilePicture isOwner={isOwner} />
+            <CardContent className={classes.cardContentLarge}>
+              <ProfileNameSection 
+                user={defaultCurrentUser}
+                isOwner={isOwner}
+                handleOptionsMenuClick={handleOptionsMenuClick}
+              />
+              <PostCountSection />
+              <NameBioSection />
+            </CardContent>
+          </Card>
+        </Hidden>
+        <Hidden smUp>
+          <Card className={classes.cardSmall}>
+            <CardContent>
+              <section className={classes.sectionSmall}>
+                <ProfilePicture size={77} isOwner={isOwner} />
+                <ProfileNameSection 
+                  user={defaultCurrentUser}
+                  isOwner={isOwner}
+                  handleOptionsMenuClick={handleOptionsMenuClick}
+                />
+              </section>
+              <NameBioSection />
+            </CardContent>
+            <PostCountSection />
+          </Card> 
+        </Hidden>
+      </div>
+    </Layout>
+  )
+};
+
+const ProfileNameSection = ({ user, isOwner, handleOptionsMenuClick }) => {
+  const classes = useProfilePageStyles();
+
+  return <div>ProfileNameSection</div>;
+};
+
+const PostCountSection = () => {
+  return <div>PostCountSection</div>;
+};
+
+const NameBioSection = () => {
+  return <div>NameBioSection</div>;
+};
 
 export default ProfilePage;
