f357d499948bb59dbf3cdde7672d9d5a7e49a384
Yuriy Puris <ypuris@itera-research.com>
2020-08-19 17:38:48 +0300

    edit profile

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,10 +1,130 @@
 import React from "react";
 import { useEditProfilePageStyles } from "../styles";
+import Layout from "../components/shared/Layout";
+import {
+  IconButton,
+  Button,
+  Hidden,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  TextField
+} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
+import { defaultCurrentUser } from "../data";
+import ProfilePicture from "../components/shared/ProfilePicture";
 
-function EditProfilePage() {
-  useEditProfilePageStyles();
+const EditProfilePage = ({ history }) => {
+  const classes = useEditProfilePageStyles();
+  const path = history.location.pathname;
+  const [showDrawer, setDrawer] = React.useState(false);
 
-  return <div>EditProfilePage</div>;
+  const handleToggleDrawer = () => {
+    setDrawer(prev => !prev);
+  };
+
+  const handleSelected = index => {
+    switch(index) {
+      case 0:
+        return path.includes('edit');
+      default:
+        break;
+    }
+  };
+
+  const handleListClick = index => {
+    switch(index) {
+      case 0:
+        history.push('/accounts/edit');
+        break;
+      default:
+        break;
+    }
+  };
+
+  const options = [
+    'Edit profile',
+    'Change Password',
+    'Apps and Websites',
+    'Email and SMS',
+    'Push Notifications',
+    'Manage Contacts',
+    'Privacy and Security',
+    'Login Activity',
+    'Emails from Instagram'
+  ];
+
+  const drawer = (
+    <List>
+      {options.map((option, index) => (
+        <ListItem
+          key={option}
+          button
+          selected={handleSelected(index)}
+          onClick={() => handleListClick(index)}
+          classes={{
+            selected: classes.listItemSelected,
+            button: classes.listItemButton
+          }}
+        >
+          <ListItemText primary={option} />
+        </ListItem>
+      ))}
+    </List>
+  )
+
+
+  return (
+    <Layout title="Edit Profile">
+      <section className={classes.section}>
+        <IconButton
+          edge="start"
+          onClick={handleToggleDrawer}
+          className={classes.menuButton}
+        >
+          <Menu />
+        </IconButton>
+        <nav>
+          <Hidden smUp implementation="css">
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={showDrawer}
+              onClose={handleToggleDrawer}
+              classes={{ paperAnchorLeft: classes.temporaryDrawer }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden
+            xsDown
+            implementation="css"
+            className={classes.permanentDrawerRoot}
+          >
+            <Drawer
+              variant="permanent"
+              open
+              classes={{
+                paper: classes.permanentDrawerPaper,
+                root: classes.permanentDrawerRoot
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main>
+          {path.includes("edit") && <EditUserInfo user={defaultCurrentUser} />}
+        </main>
+      </section>
+    </Layout>
+  );
+};
+
+const EditUserInfo = ({ user }) => {
+  return <>Edit profile</>
 }
 
 export default EditProfilePage;
